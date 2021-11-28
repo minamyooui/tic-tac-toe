7101eee5bf7f393f85c5071b975f33ff338296ee
Marty <maruri2424@gmail.com>
2021-11-27 23:18:57 -0800

    wrote player turn function that enables updating gameboard on click

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameboard = (() => {
-  const arr = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'O', 'X'];
+  const arr = [];
   const render = () => {
     const boxes = document.querySelectorAll('.block');
     boxes.forEach((e, i) => {
@@ -29,7 +29,24 @@ const Player = (name, marker) => {
 const game = (() => {
   const p1 = Player('Player1', 'X');
   const p2 = Player('Player2', 'O');
-  gameboard.render();
+  gameboard.newBoard();
+ 
+  const playerTurn = (player) => {
+    const marker = player.getMarker();
+    const boxes = document.querySelectorAll('.block');
+    function placeMarker() {
+      this.textContent = marker;
+      boxes.forEach(e => {
+        e.removeEventListener('click', placeMarker);
+      });
+    }
+    boxes.forEach(e => {
+      if (e.textContent == ' ') {
+        e.addEventListener('click', placeMarker);
+      } 
+    });
+  };
+  playerTurn(p1);
   const checkWin = () => {
     const checks = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     let win;
@@ -46,5 +63,12 @@ const game = (() => {
       return false;
     }
   }
+  // const play = (() => {
+  //   let keepGoing = true;
+  //   while(keepGoing) {
+  //     takeTurn(p1);
+  //     takeTurn(p2);
+  //   }
+  // })();
 })();
 
