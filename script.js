7b2df66b4f331d1a0cb51f127e9e10e0dde0ebb3
Marty <maruri2424@gmail.com>
2021-11-27 22:35:01 -0800

    wrote render() and styled marker boxes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const gameboard = (() => {
   const arr = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'O', 'X'];
   const render = () => {
-
+    const boxes = document.querySelectorAll('.block');
+    boxes.forEach((e, i) => {
+      e.textContent = arr[i];
+    });
   };
   const update = (marker, index) => {
     arr.splice(index, 1, marker);
@@ -14,16 +17,19 @@ const gameboard = (() => {
     }
     render();
   };
-
-  return { update, newBoard }
-
-
+  return { update, newBoard, render }
 })();
+
+const Player = (name, marker) => {
+  const getName = () => name;
+  const getMarker = () => marker;
+  return { getName, getMarker }
+};
 
 const game = (() => {
   const p1 = Player('Player1', 'X');
   const p2 = Player('Player2', 'O');
-  gameboard.newBoard();
+  gameboard.render();
   const checkWin = () => {
     const checks = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     let win;
@@ -42,8 +48,3 @@ const game = (() => {
   }
 })();
 
-const Player = (name, marker) => {
-  const getName = () => name;
-  const getMarker = () => marker;
-  return { getName, getMarker }
-};
